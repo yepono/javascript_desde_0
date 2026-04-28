b0f9887519294a48fe076e5a4dc974ae98b99064
Yepo <tonbarrz5@gamil.com>
2026-04-28 15:01:05 -0600

    Tipos de datos en js

---
+++ js/tipos-de-datos.js
@@ -0,0 +1,64 @@
+// Reto
+
+let nombre = prompt("Como te llamas? ");
+let numero = prompt("Cual es tu edad? ");
+
+console.log("Reto Tipos de datos:");
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof 0.33);
+console.log(typeof empty);
+console.log(typeof (3 === 3));
+console.log(typeof nombre);
+console.log(typeof numero);
+
+
+
+
+/// Ejercicios vistos en clase
+
+/* let mascota = "Bombon";
+console.log("Mi mascota se llama " + mascota);
+
+let nombre = prompt("Cual es tu nombre?");
+console.log("Hola " + nombre);
+
+// Tipos de datos
+let numero = 42;
+let booleano = true;
+let texto = "texto";
+console.log("");
+console.log("Numero " + numero);
+console.log("Booleano " + booleano);
+console.log("Texto " + texto);
+
+let a = 10;
+let b = 5;
+
+// Operaciones matematicas
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let divisiom = a / b;
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicacion: " + multiplicacion);
+console.log("Division: " + divisiom);
+
+// Operadores de comparacion 
+console.log("a es igual a b? " + (a === b));
+console.log("a es mayor que b? " + (a > b));
+console.log("a es menor que b " + (a < b));
+
+if (null) {
+    console.log("Esto no se ejecuta porque null es falsy")
+} else {
+    console.log("null es falsy")
+} */
